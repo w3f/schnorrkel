f4762e61d97c5803210c2119cb39672ee56a84da
Jeff Burdges <burdges@gnunet.org>
2018-11-16 19:14:53 +0100

    schnorr.rs -> ristretto.r

--- src/lib.rs
+++ src/lib.rs
@@ -278,10 +278,10 @@ extern crate serde;
 #[cfg(all(test, feature = "serde"))]
 extern crate bincode;
 
-mod schnorr;
+mod ristretto;
 
 pub mod errors;
 
 // Export everything public in ed25519.
-pub use schnorr::*;
+pub use ristretto::*;
 pub use errors::*;
