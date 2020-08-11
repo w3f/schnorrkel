87210f4854d235e314578b96fc60bfdcd48aa8ef
Isis Lovecruft <isis@patternsinthevoid.net>
2020-08-11 01:06:24 +0000

    I'm not sure what a Schnorr group is.

--- src/keys.rs
+++ src/keys.rs
@@ -552,7 +552,7 @@ impl SecretKey {
 
     /// Derive the `PublicKey` corresponding to this `SecretKey`.
     pub fn to_public(&self) -> PublicKey {
-        // No clamping in a Schnorr group
+        // No clamping necessary in the ristretto255 group
         PublicKey::from_point(&self.key * &constants::RISTRETTO_BASEPOINT_TABLE)
     }
 
