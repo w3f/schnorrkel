3bf17d162eeb15a549175f7876196026c45db162
Fiono11 <ruipedromorais11@gmail.com>
2024-05-14 11:25:17 +0100

    Add test

--- src/lib.rs
+++ src/lib.rs
@@ -248,7 +248,7 @@ pub mod derive;
 pub mod cert;
 pub mod errors;
 
-//#[cfg(all(feature = "alloc", feature = "aead"))]
+#[cfg(all(feature = "alloc", feature = "aead"))]
 pub mod olaf;
 
 #[cfg(all(feature = "aead", feature = "getrandom"))]
